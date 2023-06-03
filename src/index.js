17a68f38fc4b1da134b4e5374fee5a782b4f7a56
AdonNeet <baradontap@gmail.com>
2023-06-03 10:39:59 +0700

    minor update

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ for (const folder of commandFolders) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
 		if ('data' in command && 'execute' in command) {
-			client.commands.set(command.date.name, command);
+			client.commands.set(command.data.name, command);
 		}
 		else {
 			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property`);
